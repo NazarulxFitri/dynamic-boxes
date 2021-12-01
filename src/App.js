814171ad233f8239612ad6cc79e9d5186ccd9286
Mohd Nazarul <mohd.nazarul.fitri@gmail.com>
2021-12-01 13:07:17 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState();
+  const boxes = [];
+
+  let idx = 1;
+  for (let i = 1; i <= number; i++) {
+    idx = idx * 2;
+    let wdx = idx * 2 + "px";
+    boxes.push(<div style={{"width": wdx, "height": wdx}} id="box"></div>);
+  }
+
+  function handleChange(event) {
+    setNumber(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Dynamic Boxes</h1>
+      <p>It is advisable to enter numbers below not more than 8</p>
+      <input onChange={handleChange} />
+      <div className="boxArea">{boxes}</div>
     </div>
   );
 }
